748ace2583da08542d17c7282832d4367f8b1376
Olga <55_olga_55@mail.ru>
2022-02-21 17:01:16 +0500

    gh

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import static java.lang.Long.sum;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int speedOfPlayer1 = 5;
+        int speedOfPlayer2 = 5;
+        int speedOfPlayer3 = 2;
+
+        int income = 1;
+
+        boolean isGreenLight = false;
+
+
+        if (speedOfPlayer1 >= 1) {
+            income = income;
+            System.out.println("Green, speedOfPlayer1 выбывает:" + 0);
+        } else {
+            System.out.println("Red, speedOfPlayer1 выбывает:" + income);
+
+        }
+
+        if (speedOfPlayer2 == 0) {
+            income = income;
+            System.out.println("Red, speedOfPlayer2 выбывает:" + income);
+        } else {
+            System.out.println("Green, speedOfPlayer2 выбывает:" + 0);
+        }
+
+        if (speedOfPlayer3 >= 1) {
+            income = income;
+            System.out.println("Green, speedOfPlayer3 выбывает:" + 0);
+        } else {
+            System.out.println("Red, speedOfPlayer3 выбывает:" + income);
+
+        }
+    }
+
+}
